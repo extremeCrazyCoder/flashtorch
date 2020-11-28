6c373e917c978f48afcffae1c2aae851a683390d
extremeCrazyCoder <MKich@gmx.at>
2020-11-29 00:54:58 +0100

    Add support for maximizing input according to a wanted output

--- flashtorch/activmax/gradient_ascent.py
+++ flashtorch/activmax/gradient_ascent.py
@@ -44,7 +44,7 @@ class GradientAscent:
     # Public interface #
     ####################
 
-    def __init__(self, model, img_size=[224, 224], lr=1., use_gpu=False, channels=3, means=None, stds=None, plot=True):
+    def __init__(self, model, target_img, img_size=[224, 224], lr=1., use_gpu=False, channels=3, means=None, stds=None, plot=True):
         self.model = model
         self._img_size = img_size
         self._lr = lr
@@ -62,6 +62,10 @@ class GradientAscent:
         self.output = None
         self._use_cuda = False
         self.plot = plot
+
+        self.target_img = torch.as_tensor(target_img)
+        if torch.cuda.is_available() and self.use_gpu:
+            self.target_img = self.target_img.to('cuda')
 
     @property
     def lr(self):
@@ -203,7 +207,7 @@ class GradientAscent:
 
         self._lr = lr
 
-        if (type(filter_idxs) == int):
+        if type(filter_idxs) == int:
             self._visualize_filter(layer,
                                    filter_idxs,
                                    num_iter=num_iter,
@@ -282,7 +286,8 @@ class GradientAscent:
 
     def _register_forward_hooks(self, layer, filter_idx):
         def _record_activation(module, input_, output):
-            self.activation = torch.mean(output[:, filter_idx, :, :])
+            self.activation = torch.mean(torch.exp(torch.mul(-1,
+                  torch.abs(torch.sub(output[:, filter_idx, :, :], self.target_img)))))
 
         return layer.register_forward_hook(_record_activation)
 
